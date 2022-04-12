1329bec26399b024c77d3138f4bb37923a623062
remingtondavids <53348044+remingtondavids@users.noreply.github.com>
2022-04-12 02:11:38 -0400

    code to convert to Y channel and get DCT

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # scuffedJPEG
 trying to make a primitive JPEG-esque image compressor
+
+
+https://www.math.cuhk.edu.hk/~lmlui/dct.pdf
+
+https://www.youtube.com/watch?v=Q2aEzeMDHMA
+
+https://www.youtube.com/watch?v=0me3guauqOU
+
+https://www.youtube.com/watch?v=Kv1Hiv3ox8I
--- scuffed.py
+++ scuffed.py
@@ -1,4 +1,5 @@
 from PIL import Image;
+import math
 
 im = Image.open('test.png') # Opens the image
 im = im.convert('YCbCr')    # Converts the image to YCbCr
@@ -16,6 +17,47 @@ for i in range(rows):   # Iterates through the pixel data and adds the Y channel
 
 
 # now for the DCT o_o
+
+YChannelShifted = [[0 for x in range(cols)] for y in range(rows)]
+
+for i in range(rows):
+    for j in range(cols):
+        YChannelShifted[i][j] = YChannel[i][j] - 128
+
+DCTCoefficients = [[0 for x in range(cols)] for y in range(rows)]
+
+
+def getC(x):
+    if x == 0:
+        return 1/math.sqrt(2)
+    else:
+        return 1
+
+DCTSum = 0
+
+CCoverFour = 0 
+
+for i in range(rows):
+    for j in range(cols):
+        DCTCoefficients[i][j] = 0
+        CCoverFour = (getC(i)*getC(j))/4
+        for x in range(8):
+            for y in range(8):
+                DCTSum += YChannelShifted[i][j]*math.cos((math.pi*(2*x+1)*(i+0.5))/16)*math.cos((math.pi*(2*y+1)*(j+0.5))/16)
+        DCTCoefficients[i][j] = round(CCoverFour*DCTSum, 1)
+                
+for i in range(rows):
+    for j in range(cols):
+        print(YChannelShifted[j][i], end = " ")
+
+    print("\n")
+
+
+for i in range(rows):
+    for j in range(cols):
+        print(DCTCoefficients[j][i], end = " ")
+
+    print("\n")
 
 
 
